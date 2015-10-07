a405f71f628733734079cb90e1197cc9742a2ee7
유병훈 <isac322@naver.com>
2015-10-07 19:09:41 +0900

    add 1592.cpp

---
+++ baekjoon/baekjoon/cpp/1592.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+	int n, m, l, cnt[1000] = { 0 }, ball = 0, total = 0;
+	scanf("%d%d%d", &n, &m, &l);
+
+	while (++cnt[ball] != m) {
+		if (cnt[ball] & 1) ball = (n + ball - l) % n;
+		else ball = (ball + l) % n;
+		total++;
+	}
+
+	printf("%d", total);
+}
