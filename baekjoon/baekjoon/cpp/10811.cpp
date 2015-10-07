143ebf8b5afc051222129d1c3c039bc66f32b2f5
유병훈 <isac322@naver.com>
2015-10-07 20:08:52 +0900

    add 10811.cpp

---
+++ baekjoon/baekjoon/cpp/10811.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int n, m, arr[101] = { 0 };
+	scanf("%d%d", &n, &m);
+	for (int i = 0; i <= n; i++) arr[i] = i;
+
+	for (int i = 0; i < m; ++i) {
+		int a, b;
+		scanf("%d%d", &a, &b);
+
+		reverse(arr + a, arr + b + 1);
+	}
+	for (int i = 1; i <= n; i++) printf("%d ", arr[i]);
+}
