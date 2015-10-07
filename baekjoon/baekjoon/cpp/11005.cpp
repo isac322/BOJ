0994719f9e12a8e63baa9c46621646205d14ea09
유병훈 <isac322@naver.com>
2015-10-07 19:54:48 +0900

    add 11005.cpp

---
+++ baekjoon/baekjoon/cpp/11005.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+const char out[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+
+int main() {
+	int n, b;
+	scanf("%d%d", &n, &b);
+
+	vector<char> result;
+	while (n > 0) {
+		result.push_back(out[n % b]);
+		n /= b;
+	}
+	for (int i = result.size() - 1; i >= 0; i--) putchar(result[i]);
+}
