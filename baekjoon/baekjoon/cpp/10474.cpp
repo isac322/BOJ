60b807daa833410957ad198afd1288dc4f0515ef
유병훈 <isac322@naver.com>
2015-10-07 19:33:42 +0900

    add 10474.cpp

---
+++ baekjoon/baekjoon/cpp/10474.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int a, b;
+	while (scanf("%d%d", &a, &b), a || b) {
+		printf("%d %d / %d\n", a / b, a%b, b);
+	}
+}
