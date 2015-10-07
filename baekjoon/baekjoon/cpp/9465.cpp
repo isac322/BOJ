e3c971e6aaa9b4326f5c8454f7e6d5f31b28f5ab
유병훈 <isac322@naver.com>
2015-10-07 20:49:21 +0900

    add 9465.cpp

---
+++ baekjoon/baekjoon/cpp/9465.cpp
@@ -0,0 +1,48 @@
+#include <cstdio>
+#include <algorithm>
+
+#define NONE 0
+#define UP 1
+#define DOWN 2
+
+using namespace std;
+
+int n;
+char stiker[2][100000];
+int dp[100000][3];
+
+int solve(int start, int prev) {
+	if (start == n) return 0;
+
+	int &ret = dp[start][prev];
+	if (ret != -1) return ret;
+
+	switch (prev) {
+		case NONE:
+			ret = max(solve(start + 1, UP) + stiker[0][start], solve(start + 1, DOWN) + stiker[1][start]);
+			ret = max(ret, solve(start + 1, NONE));
+			return ret;
+
+		case UP:
+			return ret = max(solve(start + 1, DOWN) + stiker[1][start], solve(start + 1, NONE));
+
+		case DOWN:
+			return ret = max(solve(start + 1, UP) + stiker[0][start], solve(start + 1, NONE));
+	}
+}
+
+int main() {
+	int t;
+	scanf("%d", &t);
+
+	while (t--) {
+		scanf("%d", &n);
+		for (int i = 0; i < n; i++) scanf("%hhd", stiker[0] + i);
+		for (int i = 0; i < n; i++) {
+			fill_n(dp[i], 3, -1);
+			scanf("%hhd", stiker[1] + i);
+		}
+
+		printf("%d\n", solve(0, NONE));
+	}
+}
