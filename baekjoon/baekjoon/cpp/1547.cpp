4369558e3f52aa6e9c7b58d7411fbe0f495c8f39
유병훈 <isac322@naver.com>
2015-10-07 20:10:58 +0900

    add 1547.cpp

---
+++ baekjoon/baekjoon/cpp/1547.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	bool cup[4] = { false, true };
+	int n;
+	scanf("%d", &n);
+	while (n--) {
+		int a, b;
+		scanf("%d%d", &a, &b);
+		swap(cup[a], cup[b]);
+	}
+	for (int i = 1; i <= 3; i++) if (cup[i]) printf("%d", i);
+}
