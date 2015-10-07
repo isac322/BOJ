7a00f73fa19d3e5fde20a4e4c8405fac3a132228
유병훈 <isac322@naver.com>
2015-10-07 20:00:03 +0900

    update 7576.cpp

--- baekjoon/baekjoon/cpp/7576.cpp
+++ baekjoon/baekjoon/cpp/7576.cpp
@@ -4,29 +4,28 @@
 
 using namespace std;
 
-typedef pair<int, int> INTPAIR;
+typedef pair<short, short> SHRTPR;
 
-int map[1000][1000] = { 0 };
+char map[1000][1000] = { 0 };
 int main() {
 	int n, m;
-	queue< INTPAIR > que;
+	queue< SHRTPR > que;
 	scanf("%d%d", &m, &n);
 
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
-			scanf("%d", map[i] + j);
+			scanf("%hhd", map[i] + j);
 			if (map[i][j] == 1) que.push(make_pair(i, j));
 		}
 	}
 
-	bool check = true;
 	int day = 0;
 	for (; que.size() > 0; day++) {
 
 		for (int i = que.size(); i--;) {
-			INTPAIR q = que.front();
+			SHRTPR q = que.front();
 			que.pop();
-			int &r = q.first, &c = q.second;
+			short &r = q.first, &c = q.second;
 
 			r--;
 			if (r >= 0 && map[r][c] == 0) {
