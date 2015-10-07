d18ac072f2f863c87261d0ef8230600b25252294
유병훈 <isac322@naver.com>
2015-10-07 19:42:45 +0900

    update 1158.c

--- baekjoon/baekjoon/c/1158.c
+++ baekjoon/baekjoon/c/1158.c
@@ -7,12 +7,19 @@ typedef struct NODE {
 	struct NODE *next;
 }node;
 
+node *move(node *N, int n) {
+	if (n < 0) 	while (n++) N = N->prev;
+	else while (n--) N = N->next;
+	return N;
+}
+
 int main() {
-	int n, d;
+	int n, d, cnt, c;
 	int i;
 	node *start = NULL, *now = NULL, *end = NULL, *tmp;
 
 	scanf("%d %d", &n, &d);
+	cnt = n;
 	for (i = 0; i < n; i++) {
 		now = (node *) calloc(1, sizeof(node));
 		if (!i) end = start = now;
@@ -26,13 +33,13 @@ int main() {
 
 	printf("<");
 	now = start;
+	c = d > n / 2 ? 0 : 1;
 	while (now->num != now->next->num) {
-		for (i = 0; i < d - 1; i++) now = now->next;
+		now = move(now, c ? d - 1 : d - cnt-- - 1);
 		printf("%d, ", now->num);
 		tmp = now->next;
 		now->prev->next = now->next;
 		now->next->prev = now->prev;
-		free(now);
 		now = tmp;
 	}
 	printf("%d>", now->num);
