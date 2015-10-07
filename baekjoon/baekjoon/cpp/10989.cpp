8154c0cae16ece3c9f8b2a2cea946bf6df17f1fb
유병훈 <isac322@naver.com>
2015-10-07 19:49:13 +0900

    add 10989.cpp

---
+++ baekjoon/baekjoon/cpp/10989.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+	int arr[10001] = { 0 }, n, t;
+	int i = 0, j;
+	scanf("%d", &n);
+	for (; i < n; i++) {
+		scanf("%d", &t);
+		arr[t]++;
+	}
+	register char str[8];
+	for (i = 1; i < 10001; i++) {
+		sprintf(str, "%d", i);
+		for (j = 0; j < arr[i]; j++) puts(str);
+	}
+}
