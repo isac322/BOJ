c209a095dd5286bbc72c6e2a9ad95bfd7912d3db
유병훈 <isac322@naver.com>
2015-10-07 18:37:59 +0900

    update 11104.cpp

--- baekjoon/baekjoon/11104.cpp
+++ baekjoon/baekjoon/11104.cpp
@@ -1,4 +1,5 @@
 #include <cstdio>
+#include <algorithm>
 #include <bitset>
 
 using namespace std;
@@ -6,10 +7,15 @@ using namespace std;
 int main() {
 	int t;
 	scanf("%d\n", &t);
-	char str[25];
+	char ch;
+	bitset<24> set;
 	while (t--) {
-		gets(str);
-		bitset<24> set(str);
+		for (int i = 23; i >= 0; i--) {
+			ch = getchar();
+			if (ch == '0') set.reset(i);
+			else set.set(i);
+		}
 		printf("%u\n", set.to_ulong());
+		getchar();
 	}
 }
