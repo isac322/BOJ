529e8100f5d9c7b6ed71421164598be36eea1de6
유병훈 <isac322@naver.com>
2015-10-07 19:55:51 +0900

    update 8741.cpp

--- baekjoon/baekjoon/cpp/8741.cpp
+++ baekjoon/baekjoon/cpp/8741.cpp
@@ -6,9 +6,11 @@ using namespace std;
 int main() {
 	int n;
 	scanf("%d", &n);
-	char str[2000001];
+	char str[1000001];
 	fill_n(str, n, '1');
-	fill_n(str + n, n - 1, '0');
-	str[n + n - 1] = 0;
+	str[n] = 0;
+	printf("%s", str);
+	fill_n(str, n - 1, '0');
+	str[n - 1] = 0;
 	puts(str);
 }
