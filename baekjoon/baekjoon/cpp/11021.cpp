f9da228a76eb21586f3b9cd1e0ebe5ab6488d6b7
유병훈 <isac322@naver.com>
2015-10-07 19:48:44 +0900

    add 11021.cpp

---
+++ baekjoon/baekjoon/cpp/11021.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int t;
+	scanf("%d", &t);
+
+	for (int i = 1; i <= t; i++) {
+		int a, b;
+		scanf("%d %d", &a, &b);
+		printf("Case #%d: %d\n", i, a + b);
+	}
+}
