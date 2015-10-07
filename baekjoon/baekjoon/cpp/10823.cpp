f39e5ef97bcbe5ff8b03414473cf7b690610dbc6
유병훈 <isac322@naver.com>
2015-10-07 20:53:32 +0900

    add 10823.cpp

---
+++ baekjoon/baekjoon/cpp/10823.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+	char c;
+	long long sum = 0;
+	int num = 0;
+	while ((c = getchar()) != -1) {
+		switch (c) {
+			case '\n':
+				continue;
+			case ',':
+				sum += num;
+				num = 0;
+				break;
+			default:
+				num *= 10;
+				num += c - '0';
+				break;
+		}
+	}
+	printf("%lld", sum + num);
+}
